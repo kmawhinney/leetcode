10d0f80f2c43540ab069f99a653ac76698238aea
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2022-06-14 12:03:16 -0600

    Create majority_element.py

---
+++ solutions/python/majority_element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        
+        for n in nums:
+            if n in hashmap:
+                hashmap[n] += 1
+            else:
+                hashmap[n] = 1
+            
+            if hashmap[n] > len(nums)/2:
+                return n
