c520945cbc530ab8e0d728c5057859b246487a49
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2022-08-15 23:50:48 -0600

    Update group_anagrams.py

--- solutions/python/group_anagrams.py
+++ solutions/python/group_anagrams.py
@@ -1,5 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # Using DefaultDict from collections
+        from collections import defaultdict
+        anagram_dict = defaultdict(list)
+        result = []
+        
+        for s in strs:
+            sorted_s = tuple(sorted(s))
+            anagram_dict[sorted_s].append(s)
+        
+        for value in anagram_dict.values():
+            result.append(value)
+            
+        return result
+        
+        
+        # Using Regular Python Dictionary
         hashmap = {}
         result = []
         
