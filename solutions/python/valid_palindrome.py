7f27c2131d3466d8e38607863e15917ad1695369
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-01-09 15:21:21 -0800

    Update valid_palindrome.py
    Better solution

--- solutions/python/valid_palindrome.py
+++ solutions/python/valid_palindrome.py
@@ -1,23 +1,18 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new_str = ""
-        
-        for char in s.lower():
-            if char in string.ascii_letters:
-                new_str += char
-            elif char in string.digits:
-                new_str += char
+class Solution(object):
+    def isPalindrome(self, s):
+        filtered_s = ''.join(c for c in s if c.isalnum()).lower()
+
+        l = 0
+        r = len(filtered_s) - 1
+
+        if len(filtered_s) == 1:
+            return True
+
+        while l <= r:
+            if filtered_s[l] != filtered_s[r]:
+                return False
             else:
-                continue
-                
-        l = 0 # left pointer
-        r = len(new_str) - 1 # right pointer
-        
-        while l < r:
-            if new_str[l] == new_str[r]:
                 l += 1
                 r -= 1
-            else:
-                return False
         
         return True
