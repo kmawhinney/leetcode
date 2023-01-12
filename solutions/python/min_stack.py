93ec9e0502b1787a0b035e8f1717046ae5b39cfc
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-01-11 17:19:59 -0800

    Update min_stack.py
    Improved solution

--- solutions/python/min_stack.py
+++ solutions/python/min_stack.py
@@ -2,27 +2,27 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minstack = [] # minimum value when each element added
+        self.min_stack = [] # for each item added, stores the minimum at that point
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        
-        if not self.minstack:
-            self.minstack.append(val)
-        elif val < self.minstack[-1]:
-            self.minstack.append(val)
+
+        if not self.min_stack:
+            self.min_stack.append(val)
+        elif val < self.min_stack[-1]:
+            self.min_stack.append(val)
         else:
-            self.minstack.append(self.minstack[-1])
+            self.min_stack.append(self.min_stack[-1]) # current min is still min, so re-append
 
     def pop(self) -> None:
         self.stack.pop()
-        self.minstack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
-
+        
     def getMin(self) -> int:
-        return self.minstack[-1]
+        return self.min_stack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
