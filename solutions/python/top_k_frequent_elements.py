81b647dc34f3ba311f1aa60cb80b52575cff0413
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-01-15 19:16:08 -0800

    Update top_k_frequent_elements.py
    Improved solution

--- solutions/python/top_k_frequent_elements.py
+++ solutions/python/top_k_frequent_elements.py
@@ -1,22 +1,17 @@
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {} # element is key, frequency is value
-        output = []
-        
+        num_count = defaultdict(int)
+        result = []
+
         for num in nums:
-            if num in hashmap:
-                # increase frequency
-                hashmap[num] += 1
-                
-            else:
-                # add num as key, set frequency to 1
-                hashmap[num] = 1
-                
+            num_count[num] += 1
+        
+        sorted_count = sorted(num_count.items(), key=lambda x:x[1])
+
         while k > 0:
-            # find biggest frequency, add key to output, remove pair from hashmap
-            current_max = max(hashmap, key=hashmap.get)
-            output.append(current_max)
-            hashmap.pop(current_max)
+            num, count = sorted_count.pop()
+            result.append(num)
             k -= 1
-                
-        return output
+        
+        return result
