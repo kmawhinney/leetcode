76d92e76b4b8c33c583aa2a5b85f914bc32e7c4d
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-01-15 20:08:04 -0800

    Update best_time_to_buy_and_sell_stock.py

--- solutions/python/best_time_to_buy_and_sell_stock.py
+++ solutions/python/best_time_to_buy_and_sell_stock.py
@@ -1,14 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
+        l = 0
+        r = 1
         max_profit = 0
+
+        if len(prices) == 1:
+            return 0
         
-        while r <= len(prices) - 1:
-            if prices[r] > prices[l]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
+        while r < len(prices):
+            buy = prices[l]
+            sell = prices[r]
+            if sell <= buy:
+                l = r
             else:
-                l = r # move purchase to new lowest price
+                curr_profit = sell - buy
+                max_profit = max(max_profit, curr_profit)
             r += 1
-            
+        
         return max_profit
