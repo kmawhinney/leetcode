6a7d18557b7ccd1f345a51ed5b17639f1844df5a
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-01-18 16:14:41 -0800

    Update reverse_linked_list.py

--- solutions/python/reverse_linked_list.py
+++ solutions/python/reverse_linked_list.py
@@ -5,12 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
-        
+        prev = None
+        curr = head
+
         while curr:
-            downlist = curr.next
+            temp = curr.next
             curr.next = prev
             prev = curr
-            curr = downlist
+            curr = temp
         
         return prev
