e7550a2c9aae61b8e3bd26b92626b8f44dfc972c
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-08-01 01:07:58 -0700

    Update daily_temperatures.py

--- solutions/python/daily_temperatures.py
+++ solutions/python/daily_temperatures.py
@@ -1,12 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
-        answer = [0] * len(temperatures)
+        temp_stack = []
+        day_stack = []
+        result = [0] * len(temperatures)
+
+        for curr_day in range(len(temperatures)):
+            curr_temp = temperatures[curr_day]
+            while temp_stack and curr_temp > temp_stack[-1]:
+                temp_stack.pop()
+                old_day = day_stack.pop()
+                result[old_day] = curr_day - old_day
+            temp_stack.append(curr_temp)
+            day_stack.append(curr_day)
         
-        for day, temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < temp:
-                prevday = stack.pop()
-                answer[prevday] = day - prevday
-            stack.append(day)
-            
-        return answer
+        return result
