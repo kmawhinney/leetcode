15b4494b8c871d1bda3da5faf5dea9aca654d699
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-08-14 23:45:04 -0700

    Update number_of_islands.py

--- solutions/python/number_of_islands.py
+++ solutions/python/number_of_islands.py
@@ -1,3 +1,40 @@
+# BFS Approach
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = deque()
+            visited.add((r, c))
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == "1" and (nr, nc) not in visited:
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        return islands
+
+
+
+# DFS Approach
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
