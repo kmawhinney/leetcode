7d84abc7e6b0bd90bac627b87403b9dcfd41cbc7
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-08-15 09:59:14 -0700

    Update flood_fill.py

--- solutions/python/flood_fill.py
+++ solutions/python/flood_fill.py
@@ -1,3 +1,32 @@
+# BFS Approach (preferred)
+from collections import deque
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows = len(image)
+        cols = len(image[0])
+        visited = set()
+        q = deque()
+        start_color = image[sr][sc]
+        
+        q.append((sr, sc))
+        visited.add((sr, sc))
+        image[sr][sc] = color
+
+        while q:
+            r, c = q.popleft()
+            directions = [[0,1],[0,-1],[1,0],[-1,0]]
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if nr in range(rows) and nc in range(cols) and image[nr][nc] == start_color and (nr, nc) not in visited:
+                    q.append((nr, nc))
+                    visited.add((nr, nc))
+                    image[nr][nc] = color
+        
+        return image
+
+
+
+# DFS Approach
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         visited = set()
