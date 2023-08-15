0dad99016d675f9bb58251d3a8a697546a9a7ffe
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-08-15 00:00:29 -0700

    Update max_area_of_island.py

--- solutions/python/max_area_of_island.py
+++ solutions/python/max_area_of_island.py
@@ -1,3 +1,41 @@
+# BFS Approach (preferred)
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        max_area = 0
+
+        def bfs(r, c):
+            area = 0
+            q = deque()
+            q.append((r, c))
+            visited.add((r, c))
+
+            while q:
+                area += 1
+                row, col = q.popleft()
+                directions = [[0,1],[0,-1],[1,0],[-1,0]]
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 1 and ((nr, nc)) not in visited:
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+            return area
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    max_area = max(max_area, bfs(r, c))
+        return max_area
+
+
+
+# DFS Approach
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows = len(grid)
