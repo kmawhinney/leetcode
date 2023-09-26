d4b608bf6a988618fb9c4a36e1534a04c095638d
Kealan Mawhinney <91577912+kmawhinney@users.noreply.github.com>
2023-09-25 23:41:00 -0700

    Update reorder_list.py

--- solutions/python/reorder_list.py
+++ solutions/python/reorder_list.py
@@ -5,24 +5,20 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        slow, fast = head, head.next
+        # Find second-half of linked list
+        slow, fast = head, head
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        second_half = slow.next
-        slow.next = None
-        prev = None
-        while second_half:
-            temp = second_half.next
-            second_half.next = prev
-            prev = second_half
-            second_half = temp
+        # Reverse second-half of linked list
+        curr, prev = slow, None
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
         
-        begin, end = head, prev
-        
-        while end:
-            temp1, temp2 = begin.next, end.next
-            begin.next = end
-            end.next = temp1
-            begin, end = temp1, temp2
+        # Merge both halves
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
